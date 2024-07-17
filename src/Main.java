2ad0de0eaf09eb45a5fc2dd10990f473b62546c3
yousefnegmeldin <yousefnegm@gmail.com>
2024-07-17 16:02:36 +0300

    slight update to main class

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,16 @@ public class Main {
         SQLService sqlService = new SQLService();
 
         //example author that is already found in database;
-        Author author1 = sqlService.getAuthorByName("Julie","Myriam");
+        //make sure to choose author name thats already found in database
+        Author author1 =  new Author("JK","Rowling",30);
+        sqlService.addAuthorToDatabase(author1);
 
         //get all books
         ArrayList<Book> allBooks = sqlService.getAllBooks();
-        System.out.println("Printing all books!");
-        for(Book book: allBooks){
-            System.out.println(book);
-        }
+//        System.out.println("Printing all books!");
+//        for(Book book: allBooks){
+//            System.out.println(book);
+//        }
 
         //delete book
         Book bookToDelete = allBooks.get(allBooks.size()-1);
@@ -35,23 +37,22 @@ public class Main {
 
         //find book by name or by isbn
         Book selectedBookByName = sqlService.getBookByName("I am number four");
-        System.out.println(selectedBookByName);
+        System.out.println("Selected book by name: "+ selectedBookByName);
         Book selectedBookByIsbn = sqlService.getBookByIsbn("273589632586325");
-        System.out.println(selectedBookByIsbn);
+        System.out.println("Selected book by isbn: "+ selectedBookByIsbn);
 
 
         //find books by author
         ArrayList<Book> retrievedBooksByAuthor = sqlService.retrieveBooksByAuthor(author1);
         if(retrievedBooksByAuthor.size()>0) {
             for (Book book : retrievedBooksByAuthor) {
-                System.out.println(book);
+                System.out.println("Book by author :"+author1.getFirstName()+" " +book);
             }
         }
         //borrow and return book
         Customer firstCustomer = new Customer("youseCustomer", "negm",19);
         sqlService.addCustomerToDatabase(firstCustomer);
         sqlService.addCustomerIdToBook(bookToBeAdded, firstCustomer);
-
         sqlService.removeCustomerIdFromBook(bookToBeAdded);
 
 
